b53b79146e359a2f4990cc9953ead769d7183aa6
pwh2000 <panweihua2000@gmail.com>
2023-03-11 08:27:28 +0000

    feat: add comment

--- element.sh
+++ element.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [[ -z "$1" ]]
 then 
   echo "Please provide an element as an argument."
 else
+  # check what type the argument is
+
+  # if atomic_number
+
+  # else if symbol
+
+  # else then name
+  
 fi
