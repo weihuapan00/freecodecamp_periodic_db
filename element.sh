ca98bc5b73df0a5e4214e57dd27b4b9afeb3674a
pwh2000 <panweihua2000@gmail.com>
2023-03-11 09:42:46 +0000

    feat: add if statement

--- element.sh
+++ element.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z "$1" ]]
-then 
+then
   echo "Please provide an element as an argument."
+
 else
   # check what type the argument is
 
   # if atomic_number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    echo "number" 
 
-  # else if symbol
-
-  # else then name
-  
+    # else if symbol
+  elif [[ $1 =~ ^[A-Z][a-z]{0,4}$ ]]
+  then
+    echo "symbol"
+    # else then name
+  elif [[ $1 =~ ^[A-Z][a-z]*$ ]]
+  then
+    echo "name"
+    # not recognize
+  else
+    echo "idk"
+  fi
 fi
