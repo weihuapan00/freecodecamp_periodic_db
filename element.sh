2035ce5d3b5bb2f2b22805bdd46007be6c74f4b4
pwh2000 <panweihua2000@gmail.com>
2023-03-11 09:54:46 +0000

    feat: comment if use sql or instead

--- element.sh
+++ element.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t  -c"
 
 if [[ -z "$1" ]]
 then
   echo "Please provide an element as an argument."
 
 else
+  # sql query use or condition
+  RESULT=$($PSQL "select * from elements where atomic_number = $1 or symbol = '$1' or name = '$1'")
+  echo "$RESULT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  done
+
+
+
   # check what type the argument is
 
-  # if atomic_number
-  if [[ $1 =~ ^[0-9]+$ ]]
-  then
-    echo "number" 
-
-    # else if symbol
-  elif [[ $1 =~ ^[A-Z][a-z]{0,4}$ ]]
-  then
-    echo "symbol"
-    # else then name
-  elif [[ $1 =~ ^[A-Z][a-z]*$ ]]
-  then
-    echo "name"
-    # not recognize
-  else
-    echo "idk"
-  fi
+  # # if atomic_number
+  # if [[ $1 =~ ^[0-9]+$ ]]
+  # then
+  #   RESULT=$($PSQL "select * from elements where atomic_number = $1")
+  #   echo "number: $RESULT" 
+  #   # else if symbol
+  # elif [[ $1 =~ ^[A-Z][a-z]{0,4}$ ]]
+  # then
+  #   echo "symbol"
+  #   # else then name
+  # elif [[ $1 =~ ^[A-Z][a-z]*$ ]]
+  # then
+  #   echo "name"
+  #   # not recognize
+  # else
+  #   echo "idk"
+  # fi
 fi
